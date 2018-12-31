8c74ff75c237b472df7bfae0e30f0b5c259824c4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-31 18:47:15 +0000

    Deploy hudovisk/udacity-final-project to github.com/hudovisk/udacity-final-project.git:gh-pages

---
+++ precache-manifest.8fa4e6af4e3d3f73fd20d70e04b6ec32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba9a9ddccfd1b608af5c",
+    "url": "/udacity-final-project/static/css/main.6ef6133d.chunk.css"
+  },
+  {
+    "revision": "ba9a9ddccfd1b608af5c",
+    "url": "/udacity-final-project/static/js/main.ba9a9ddc.chunk.js"
+  },
+  {
+    "revision": "d88eb7f5add389a25547",
+    "url": "/udacity-final-project/static/js/1.d88eb7f5.chunk.js"
+  },
+  {
+    "revision": "39b881749ff0705515f4",
+    "url": "/udacity-final-project/static/js/runtime~main.39b88174.js"
+  },
+  {
+    "revision": "4748a8276906da17a5fe49b37ecd7295",
+    "url": "/udacity-final-project/index.html"
+  }
+];
